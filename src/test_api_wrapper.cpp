564d4bb78f3eea8ff13fa353e6b9a0924e872f93
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-11-01 22:09:45 +0100

    Adding licence information

--- src/test_api_wrapper.cpp
+++ src/test_api_wrapper.cpp
@@ -1,3 +1,23 @@
+/*
+    This file is part of osm_diff_analyzer_test_API, Openstreetmap
+    diff analyzer whose aim is to test API allowing to analyzers to get
+    OSM object information through OSM API implementation provided by
+    diff analyzis framewwork
+    Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include "test_api_wrapper.h"
 #include "test_api_analyzer_description.h"
 #include "test_api_analyzer.h"
